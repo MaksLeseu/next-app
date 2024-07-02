4da2dc151f5e787b4085e12fd56b9bec0d6e0fcb
Maxim Lesev <lesov0iblr@gmail.com>
2024-07-02 14:04:16 +0200

    Create a request

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,9 +1,34 @@
-const Characters = () => {
-    return (
-        <div>
-            Characters
-        </div>
-    );
+import {API} from "@/assets/api";
+import {CharacterType, ResponseType} from "@/assets/rick-and-morty-api";
+
+
+export async function getData() {
+    const res = await fetch('https://rickandmortyapi.com/api/character', {})
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+const Characters = async () => {
+    try {
+        const data = await getData();
+
+        const characters = data.results.map((ch: any) => (
+            <div key={ch.id}>{ch.name}</div>
+        ))
+
+        return (
+            <div>
+                Characters
+                {characters}
+            </div>
+        );
+    } catch (error) {
+        return <div>Error: {(error as Error).message}</div>;
+    }
 };
 
 export default Characters;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from "./page.module.css";
+import Characters from "@/app/characters/page";
 
 export default function Home() {
   return (
     <main>
       Hello World
+
     </main>
   );
 }
